d707f924c23a14d079d66395575e930d6efa08b8
Lucy Barker <lucy@geniemouse.com>
2019-09-20 14:19:16 +0100

    Updates

---
+++ precache-manifest.2048601e52d20d2903aa97c22df5a944.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adfb0dc2065b8791dbefced26b31616",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "302e66dab893bf155d5f",
+    "url": "/spacex/static/css/main.8a31cd00.chunk.css"
+  },
+  {
+    "revision": "b6bb915780143c70720f",
+    "url": "/spacex/static/js/2.9cc136f0.chunk.js"
+  },
+  {
+    "revision": "302e66dab893bf155d5f",
+    "url": "/spacex/static/js/main.1a437d38.chunk.js"
+  },
+  {
+    "revision": "d97e5a4bb81ec134a109",
+    "url": "/spacex/static/js/runtime~main.a706bf65.js"
+  },
+  {
+    "revision": "0b59275f161f6f43f0ca3b2365a77f77",
+    "url": "/spacex/static/media/BrandonGrotesque-Bold.0b59275f.ttf"
+  },
+  {
+    "revision": "1ad8d40bebcdcc1f79f0e22ce35b3d75",
+    "url": "/spacex/static/media/BrandonGrotesque-Bold.1ad8d40b.woff"
+  },
+  {
+    "revision": "441b33aac91c0f5883e390134d6fa6eb",
+    "url": "/spacex/static/media/BrandonGrotesque-Bold.441b33aa.woff2"
+  },
+  {
+    "revision": "013a89eb356fb88baad2f6178c5400ba",
+    "url": "/spacex/static/media/BrandonGrotesque-Regular.013a89eb.woff"
+  },
+  {
+    "revision": "aba7d12553de107511905eb7595a72d9",
+    "url": "/spacex/static/media/BrandonGrotesque-Regular.aba7d125.ttf"
+  },
+  {
+    "revision": "aff015dfe96d4c9021ace79558be225d",
+    "url": "/spacex/static/media/BrandonGrotesque-Regular.aff015df.woff2"
+  },
+  {
+    "revision": "10cc8d8755a6ebdc69c8e4fa006de6fc",
+    "url": "/spacex/static/media/BrandonGrotesque-RegularItalic.10cc8d87.woff"
+  },
+  {
+    "revision": "5267a22825e445ebb40686639b9c1cd8",
+    "url": "/spacex/static/media/BrandonGrotesque-RegularItalic.5267a228.woff2"
+  },
+  {
+    "revision": "ba6abfddb0c1143b43d41ce9bc6fc236",
+    "url": "/spacex/static/media/BrandonGrotesque-RegularItalic.ba6abfdd.ttf"
+  },
+  {
+    "revision": "23a1b6b898599bb6c747d800291f4537",
+    "url": "/spacex/static/media/launch-home.23a1b6b8.png"
+  },
+  {
+    "revision": "b7cae1c6d33ed49312f7553a33380af7",
+    "url": "/spacex/static/media/launch-home@2x.b7cae1c6.png"
+  },
+  {
+    "revision": "59668b510e3b4413e6865a5f5ddf1d42",
+    "url": "/spacex/static/media/launch-home@3x.59668b51.png"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/spacex/static/media/spacex-logo.950ba665.png"
+  }
+]);
